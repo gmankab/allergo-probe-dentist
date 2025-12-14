eeaad340201b50d37c67337341ba064fb3470425
gmanka <gmankab@gmail.com>
2025-12-14 09:34:22 +0500

    [u] ignore deprecation warning from asyncio/flet

--- allergo/config.py
+++ allergo/config.py
@@ -1,7 +1,11 @@
 import flet
+import warnings
 
 
 card_height: int = 325
 border_radius=30
 padding=24
 page: flet.Page
+
+warnings.simplefilter('ignore', DeprecationWarning)
+# this line should be removed after it fixed in flet
